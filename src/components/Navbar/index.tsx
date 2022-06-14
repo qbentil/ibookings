90404764e4fe84af60b2a811c693cb24a152947c
qbentil <bentilshadrack72@gmail.com>
2022-06-14 02:35:50 +0000

    Update navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,13 +4,16 @@ import { useState } from 'react'
 import { Link } from 'react-router-dom';
 import Useractions from './Useraction';
 import LoginAction from './Loginaction';
+import Navigations from '../Navigations';
 
 const Navbar = () => {
   const [user, setUser] = useState(null)
   
-  const logout = () => {}
+  const logout = () => {
+    setUser(null)
+  }
   return (
-    <div className="w-full bg-blue- fixed z-50 flex justify-center items-center backdrop-blur-md md:p-3 md:px-4 lg:p-6 lg:px-16">
+    <div className="w-full z-50  flex flex-col gap-3 md:gap-6 justify-center items-center backdrop-blur-md md:p-3 md:px-4 lg:p-6 lg:px-16">
         {/* desktop & Tablet view */}
         <div className="flex w-full md:w-[80%] justify-between  py-2 px-5 md:p-0 items-center">
         <Link to={"/"}>
@@ -20,7 +23,7 @@ const Navbar = () => {
           >
             {/* <img src={Logo} alt="Logo" className="md:w-6 lg:w-8 object-cover" /> */}
             <p className="text-headerColor font-brand text-xl lg:text-2xl font-bold">
-              iBookings
+              i<span className='text-blue-800'>Bookings</span>
             </p>
           </motion.div>
         </Link>
@@ -29,6 +32,7 @@ const Navbar = () => {
           user ? <Useractions user={user} action={logout} />: <LoginAction text='Login'  />
         }
         </div>
+        <Navigations />
     </div>
   )
 }
