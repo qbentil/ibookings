3d4f647c4c015567ea44426084ed1f7b6885a968
qbentil <bentilshadrack72@gmail.com>
2022-06-16 15:25:21 +0000

    Update Showcase component

--- src/components/Showcase/index.tsx
+++ src/components/Showcase/index.tsx
@@ -2,7 +2,7 @@ import QuickSearch from "../Quicsearch";
 
 const Showcase = () => {
   return (
-    <div className="w-full md:w-[99%] bg-blue-700 h-80 flex flex-col items-center justify-center relative">
+    <div className="w-full md:w-[99%] bg-blue-700 h-80 flex flex-col items-center justify-center relative mx-auto mt-5 md:mt-0">
       <div className="w-full p-5 py-10 flex flex-col gap-5 items-center md:items-start justify-center">
         <p className="md:text-3xl text-white font-semibold">
           A lifetime of discounts? It's Genuis.
