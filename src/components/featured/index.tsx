2e405321be9079b5aad52b7dc33b9ae60d24285b
qbentil <bentilshadrack72@gmail.com>
2022-06-16 15:24:52 +0000

    Create Featured Rooms components

---
+++ src/components/featured/index.tsx
@@ -0,0 +1,16 @@
+
+const Featured = () => {
+  return (
+    <div className="">
+        <div className="">
+            <img src="https://t-cf.bstatic.com/xdata/images/xphoto/600x400/140018375.webp?k=d2d3a8f5a02e0d85afc4aa94078ec47e23d4c57981f518af4f6fa7cc172adb86&o=" alt="" />
+            <div className="">
+                <h1>Pokuase</h1>
+                <h1>123 Properties</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Featured
