bb20e0a53d6b9b3d5ead8d139ffc5a8ccd5b211a
qbentil <bentilshadrack72@gmail.com>
2022-06-16 15:25:09 +0000

    Render featured comppnent on home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,9 @@
-import { Showcase } from "../../components"
+import { Featured } from "../../components"
 
 const Home = () => {
   return (
-    <div className="w-full flex flex-col items-center justify-center">
-      <Showcase />
+    <div className="w-full flex flex-col items-center justify-center gap-8 mt-5">
+      <Featured />
     </div>
   )
 }
